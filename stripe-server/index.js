a3091622780b6cd16329337b443b8fdd5df202e5
lets-dancing <2010pimenoff@gmail.com>
2024-01-12 10:45:37 +0300

    stripe testing

--- stripe-server/index.js
+++ stripe-server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST || process.env.STRIPE_SECRET_LIVE);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
