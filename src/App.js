9c90b068c8c6b85547849dfb74b7acc9bb7d8449
lets-dancing <2010pimenoff@gmail.com>
2024-01-23 13:44:54 +0300

    some updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import './loader.css'
 import NavBar from './Components/NavBar/NavBar';
 import BtnToTop from './addons/BtnToTop';
-import SimpleForm from './addons/moduleChatBot/SimpleForm';
-import { useState } from 'react';
+// import SimpleForm from './addons/moduleChatBot/SimpleForm';
+// import { useState } from 'react';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
   return (
     <div className='App'>
       {/* <button className='btnChatBot' onClick={() => setIsOpen(!isOpen)}><img src={'./gif-tg.gif'} alt='chatbot'/></button>
