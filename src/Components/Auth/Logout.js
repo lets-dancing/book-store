e0ba8aad1e05af3b06e8fc116168a563d7c4b6de
lets-dancing <2010pimenoff@gmail.com>
2024-01-23 13:05:25 +0300

    some updates

--- src/Components/Auth/Logout.js
+++ src/Components/Auth/Logout.js
@@ -1,23 +1,14 @@
 // Компонент Logout для выхода пользователя из системы с использованием Auth0
-import React, { useState } from "react";
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Logout = () => {
     const { logout } = useAuth0();
-    const [showPopup, setShowPopup] = useState(false);
-
     return (
         <div className="navBarIcons">
-            <button className="logoutBtn" onClick={() => {setShowPopup(true)}}>
+            <button className="logoutBtn" onClick={() => logout()}>
                 <img className="logoutIcon" src={`./logout.png`} alt="logoutIcon"/>
             </button>
-            {showPopup && 
-                <div className="popup">
-                    <p>Вы уверены, что хотите выйти?</p>
-                    <button onClick={() => logout()}>Да</button>
-                    <button onClick={() => setShowPopup(false)}>Нет</button>
-                </div>
-            }
         </div>
     );
 };
