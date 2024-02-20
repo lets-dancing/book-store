5c7f04505ed6282b78271c5377063740df2effab
lets-dancing <2010pimenoff@gmail.com>
2024-02-20 10:26:38 +0300

    add comments in simpleform

--- src/addons/moduleChatBot/SimpleForm.js
+++ src/addons/moduleChatBot/SimpleForm.js
@@ -1,8 +1,12 @@
+// Этот React компонент реализует простую форму чата с использованием библиотеки react-simple-chatbot. 
+// Пользователь может открыть чат, ответить на вопросы бота о своем имени, возрасте и любимой книге. 
+// Данные проверяются на корректность, и пользователь может обновить свои ответы. 
+// Для закрытия чата используется кнопка с иконкой крестика. 
+// Также реализовано закрытие чата при нажатии клавиши Escape.
 import React, { useEffect, useState } from 'react'
 import ChatBot from 'react-simple-chatbot';
 import Review from './Review';
 import { FaTimes } from 'react-icons/fa';
-
 function SimpleForm() {
     const [isOpen, setIsOpen] = useState(false);
     useEffect(() => {
@@ -14,121 +18,115 @@ function SimpleForm() {
             document.addEventListener('keydown', handleKeyDown);
             return () => document.removeEventListener('keydown', handleKeyDown);
         }, [setIsOpen]);
-        
         if (!isOpen) return (
             <button className='btnChatBot' onClick={() => setIsOpen(!isOpen)}><img src={'./gif-tg.gif'} alt='chatbot'/></button>
         );
-        return (
-            <div className='modalChat'>
-            <button className='closeChatBot' onClick={() => setIsOpen(false)}>
-                <FaTimes />
-            {/* <img src={`./cross.png`} alt="close modal" className="closeChatBot" onClick={() => setIsOpen(false)}/> */}
-            </button>
-            <ChatBot
-                steps={[
-                    {
-                        id: '1',
-                        message: 'Как вас зовут?',
-                        trigger: 'name',
+    return (
+        <div className='modalChat'>
+        <button className='closeChatBot' onClick={() => setIsOpen(false)}>
+            <FaTimes />
+        </button>
+        <ChatBot
+            steps={[
+                {
+                    id: '1',
+                    message: 'Как вас зовут?',
+                    trigger: 'name',
+                },
+                {
+                    id: 'name',
+                    user: true,
+                    trigger: '3',
+                },
+                {
+                    id: '3',
+                    message: 'Привет {previousValue}! Сколько вам лет?',
+                    trigger: 'age',
+                },
+                {
+                    id: 'age',
+                    user: true,
+                    trigger: '5',
+                    validator: (value) => {
+                        if (isNaN(value)) {
+                            return 'укажите число';
+                        } else if (value < 0) {
+                            return 'минус не надо';
+                        } else if (value > 120) {
+                            return `${value}? Да ладно!`;
+                        }
+                        return true;
                     },
-                    {
-                        id: 'name',
-                        user: true,
-                        trigger: '3',
-                    },
-                    {
-                        id: '3',
-                        message: 'Привет {previousValue}! Сколько вам лет?',
-                        trigger: 'age',
-                    },
-                    {
-                        id: 'age',
-                        user: true,
-                        trigger: '5',
-                        validator: (value) => {
-                            if (isNaN(value)) {
-                                return 'укажите число';
-                            } else if (value < 0) {
-                                return 'минус не надо';
-                            } else if (value > 120) {
-                                return `${value}? Да ладно!`;
-                            }
-
-                            return true;
-                        },
-                    },
-                    {
-                        id: '5',
-                        message: 'Какая ваша любимая книга?',
-                        trigger: 'book',
-                    },
-                    {
-                        id: 'book',
-                        user: true,
-                        trigger: '7',
-                    },
-
-                    {
-                        id: '7',
-                        message: 'Отлично! Еще раз проверьте информацию.',
-                        trigger: 'review',
-                    },
-                    {
-                        id: 'review',
-                        component: <Review />,
-                        asMessage: true,
-                        trigger: 'update',
-                    },
-                    {
-                        id: 'update',
-                        message: 'Хотите уточнить информацию?',
-                        trigger: 'update-question',
-                    },
-                    {
-                        id: 'update-question',
-                        options: [
-                            { value: 'yes', label: 'Да', trigger: 'update-yes' },
-                            { value: 'no', label: 'Нет', trigger: 'end-message' },
-                        ],
-                    },
-                    {
-                        id: 'update-yes',
-                        message: 'Что именно хотите уточнить?',
-                        trigger: 'update-fields',
-                    },
-                    {
-                        id: 'update-fields',
-                        options: [
-                            { value: 'name', label: 'Имя', trigger: 'update-name' },
-                            { value: 'book', label: 'Книга', trigger: 'update-book' },
-                            { value: 'age', label: 'Возраст', trigger: 'update-age' },
-                        ],
-                    },
-                    {
-                        id: 'update-name',
-                        update: 'name',
-                        trigger: '7',
-                    },
-                    {
-                        id: 'update-book',
-                        update: 'book',
-                        trigger: '7',
-                    },
-                    {
-                        id: 'update-age',
-                        update: 'age',
-                        trigger: '7',
-                    },
-                    {
-                        id: 'end-message',
-                        message: 'Спасибо за информацию!',
-                        end: true,
-                    },
-                ]}
-            />
-        </div>
-        );
-    }
-
-
+                },
+                {
+                    id: '5',
+                    message: 'Какая ваша любимая книга?',
+                    trigger: 'book',
+                },
+                {
+                    id: 'book',
+                    user: true,
+                    trigger: '7',
+                },
+                {
+                    id: '7',
+                    message: 'Отлично! Еще раз проверьте информацию.',
+                    trigger: 'review',
+                },
+                {
+                    id: 'review',
+                    component: <Review />,
+                    asMessage: true,
+                    trigger: 'update',
+                },
+                {
+                    id: 'update',
+                    message: 'Хотите уточнить информацию?',
+                    trigger: 'update-question',
+                },
+                {
+                    id: 'update-question',
+                    options: [
+                        { value: 'yes', label: 'Да', trigger: 'update-yes' },
+                        { value: 'no', label: 'Нет', trigger: 'end-message' },
+                    ],
+                },
+                {
+                    id: 'update-yes',
+                    message: 'Что именно хотите уточнить?',
+                    trigger: 'update-fields',
+                },
+                {
+                    id: 'update-fields',
+                    options: [
+                        { value: 'name', label: 'Имя', trigger: 'update-name' },
+                        { value: 'book', label: 'Книга', trigger: 'update-book' },
+                        { value: 'age', label: 'Возраст', trigger: 'update-age' },
+                    ],
+                },
+                {
+                    id: 'update-name',
+                    update: 'name',
+                    trigger: '7',
+                },
+                {
+                    id: 'update-book',
+                    update: 'book',
+                    trigger: '7',
+                },
+                {
+                    id: 'update-age',
+                    update: 'age',
+                    trigger: '7',
+                },
+                {
+                    id: 'end-message',
+                    message: 'Спасибо за информацию!',
+                    end: true,
+                },
+            ]}
+        />
+    </div>
+    );
+}
 export default SimpleForm;
